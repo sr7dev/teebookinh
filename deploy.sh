1af23e8e6998d98328a3da2b7d805ef6847107e0
giovanni <giovannijb11@gmail.com>
2022-01-11 20:35:33 -0500

    update deploy workflow

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ echo "Start script."
 
 service apache2 restart
 
-cd /var/www/laravel-ksr-boston_abundance/
+cd /var/www/backteehive/
+php artisan migrate
 php artisan l5-swagger:generate
 
 echo "End script."
